34adbed516c275f442c419517833ea7eec5d8da5
Maria Sandu <Maria.Sandu@emids.com>
2023-09-15 15:50:52 +0300

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Maria Sandu on 15.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
